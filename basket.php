40c65ea31dbd675d375596a1e0f762ec048e5410
Camditta <93238453+Camditta@users.noreply.github.com>
2023-03-09 05:16:03 +0000

    Basket Updates

--- basket.php
+++ basket.php
@@ -9,6 +9,17 @@ echo "<body>";
 include ("headfile.html");      //include header layout file
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
 
+//if the value of the product id to be deleted (which was posted through the hidden field) is set
+if (isset($_POST['del_prodid']))
+{
+//capture the posted product id and assign it to a local variable $delprodid
+$delprodid=$_POST['del_prodid'];
+//unset the cell of the session for this posted product id variable
+unset ($_SESSION['basket'][$delprodid]);
+//display a "1 item removed from the basket" message
+echo " <p class='updateInfo'><b>1 item removed</b></p>";
+}
+
 //if the posted ID of the new product is set i.e. if the user is adding a new product into the basket
 if (isset($_POST['h_prodid']))
 {
@@ -39,7 +50,7 @@ $total=0;
 
 echo "<p><table id='baskettable'>";
 echo "<tr>";
-echo "<th>Product Name</th><th>Price</th><th>Quantity</th><th>Subtotal</th>";
+echo "<th>Product Name</th><th>Price</th><th>Quantity</th><th>Subtotal</th><th>Remove Product</th>";
 echo "</tr>";
 
 if (isset($_SESSION['basket']))
@@ -59,6 +70,12 @@ if (isset($_SESSION['basket']))
         //calculate subtotal, store it in a local variable $subtotal and display it
         $subtotal=$arrayp['prodPrice'] * $value;
         echo "<td>&pound".number_format($subtotal,2)."</td>";
+        echo "<form action='basket.php' method=post>";
+        echo "<td>";
+        echo "<input type='submit' value='Remove' id='submitbtn'>";
+        echo "</td>";
+        echo "<input type='hidden' name='del_prodid' value=".$arrayp['prodId'].">";
+        echo "</form>";
         echo "</tr>";
         //increase total by adding the subtotal to the current total
         $total=$total+$subtotal;
@@ -74,12 +91,16 @@ else
 
 // Display total
 echo "<tr>";
-echo "<td colspan=3><b>TOTAL</b></td>";
+echo "<td colspan=4><b>TOTAL</b></td>";
 echo "<td><b>&pound".number_format($total,2)."</b></td>";
 echo "</tr>";
 echo "</table>";
 
 echo "<br><p class='updateInfo'><a href='clearbasket.php'>CLEAR BASKET</a></p>";
+
+echo "<br><p class='updateInfo'>New homteq Customer?<a href='signup.php'> Sign Up</a></p>";
+echo "<br><p class='updateInfo'>Already Have An Account?<a href='login.php'> Login</a></p>";
+
 
 include("footfile.html"); //include head layout
 echo "</body>";
